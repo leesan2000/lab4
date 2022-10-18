69cfc8dbbe55a1678727bc3767542fce1a7035fa
leesan2000 <lisandrovicens1@gmail.com>
2022-10-18 19:48:13 +0000

    creacion tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,18 @@
 class TweetsController < ApplicationController
     def show
         @tweet = Tweet.find(params[:id])
-      end
-      def index
+    end
+      
+    def index
         @tweets = Tweet.all.order(created_at: :desc)
-      end
+    end
+
+    def create
+        @tweet = Tweet.new(params.require(:tweet).permit(:content, :monster_id))
+        if @tweet.save
+            redirect_to @tweet, notice: "Tweet creado exitosamente"
+        else
+            render :new
+        end
+    end
 
